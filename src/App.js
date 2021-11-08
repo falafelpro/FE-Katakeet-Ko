1d1d7b3fe25cad6da5c4bec2dad192d2da4fd8f1
falafelpro <47983933+falafelpro@users.noreply.github.com>
2021-11-08 19:52:49 +0300

    Hi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import CategoryList from "./components/CategoryList";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div class="container-fluid">
+        <CategoryList />
+      </div>
     </div>
   );
 }
--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,7 +1,13 @@
 import React from "react";
+import CategoryItem from "./CategoryItem";
 
 function CategoryList() {
-  return <div></div>;
+  return (
+    <div>
+      <div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
+        <CategoryItem />
+      </div>
+    </div>
+  );
 }
-
 export default CategoryList;
