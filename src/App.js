db576e1af5231dd06ca343fe8284f23ed01393a4
CaptSoulBeard <76257198+CaptSoulBeard@users.noreply.github.com>
2021-11-10 08:24:42 +0300

    Commiting because I have to delete the project due to an annoying error >:(

--- src/App.js
+++ src/App.js
@@ -5,17 +5,7 @@ import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 
-// import IngredientModal from "./components/IngredientModal";
-// import { React, useState } from "react";
-// import { Button } from "react-bootstrap";
-// import authStore from "./stores/authstore";
-// import { observer } from "mobx-react";
-
 function App() {
-  // const [isOpen, setIsOpen] = useState(false);
-  // const closeModal = () => setIsOpen(false);
-  // const openModal = () => setIsOpen(true);
-
   return (
     <div className="App">
       <NavBar />
@@ -26,12 +16,6 @@ function App() {
       <div class="container-fluid">
         <CategoryList />
       </div>
-      {/* <div>
-        {authStore.user ? (
-          <Button onClick={openModal}>Add Ingredients</Button>
-        ) : null}
-        <IngredientModal isOpen={isOpen} closeModal={closeModal} />
-      </div> */}
     </div>
   );
 }
